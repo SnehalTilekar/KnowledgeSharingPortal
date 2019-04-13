6745f42d8df8bd11c37dfd20b897ad4b2b6b67eb
SnehalTilekar <snehal8237@gmail.com>
2019-04-13 15:27:42 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   firebase:
   {
-    apiKey: "AIzaSyBuQE6dYBHlTa3parfjHHZG1oGRFg1p4SU",
-    authDomain: "fir-demoapp1-f5235.firebaseapp.com",
-    databaseURL: "https://fir-demoapp1-f5235.firebaseio.com",
-    projectId: "fir-demoapp1-f5235",
-    storageBucket: "fir-demoapp1-f5235.appspot.com",
-    messagingSenderId: "871722294649"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
