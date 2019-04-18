03095952c1d842585f18482efb52f2327abe76b8
Snehal Tilekar <snehal8237@gmail.com>
2019-04-18 17:19:43 +0530

    Remove duplicated directory

--- src2/app/projects/project-start/project-start.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-project-start',
-  templateUrl: './project-start.component.html'
-})
-export class ProjectStartComponent
-{
-}
